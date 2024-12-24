2c2cae61817d38fd82b062e48583eb86e1512841
Atishay Lahri <atishay.lahri@gmail.com>
2024-12-23 21:46:59 -0500

    Initial Commit

---
+++ SimpleBeam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleBeam
+//
+//  Created by Atishay Lahri on 12/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleBeam/SimpleBeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBeamApp.swift
+//  SimpleBeam
+//
+//  Created by Atishay Lahri on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
